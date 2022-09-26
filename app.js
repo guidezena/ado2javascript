0f307c3193d5a809a45bf589f7ba379b1e99e2c1
guideze <guilhermepereiradezena@gmail.com>
2022-09-26 18:14:27 -0300

    Ado finalizada

--- app.js
+++ app.js
@@ -7,16 +7,18 @@ function get(url) {
 function api() {
     let route = get("https://api.agify.io/?name[]=michael&name[]=matthew&name[]=jane")
     data = JSON.parse(route)
-    let usuario = prompt('Qual o seu nome?')
-    if (usuario == "michael") {
-        alert('A idade do michael é ' + data[0].age)
-    } else if (usuario == "matthew") {
-        alert('A idade do matthew é ' + data[1].age)
-    } else if (usuario == "jane") {
-        alert('A idade do jane é ' + data[2].age)
-    } else {
-        alert('Não existe esse nome')
+
+    var currentAge 
+    let user = prompt('Qual o seu nome?')
+    data.forEach(element => {
+        if(element.name == user){
+            currentAge = element.age
+            alert("A idade do usuario " + element.name + " é: " + element.age )
+        }
+    });
+
+    if(!currentAge) {
+        alert('Não tem esse usuario cadastrado')
     }
-console.log(data)
 }
 api()
